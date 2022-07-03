aad6b821470f5e338398718fc620e74cd816bce1
Sattvik Palta <sattvikpalta@gmail.com>
2022-07-03 12:14:11 -0400

    Adds db connectivity

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
+const { Db } = require("pg");
 
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
