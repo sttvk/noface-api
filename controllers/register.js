a17301e6b7a68464e61262b87697cd912d2326e3
Sattvik Palta <sattvikpalta@gmail.com>
2022-07-03 13:41:30 -0400

    Adds db connectivity

--- controllers/register.js
+++ controllers/register.js
@@ -1,3 +1,13 @@
+const knex = require("knex");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  },
+});
+
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
